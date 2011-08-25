be547bfa2918e71785a18eb8eaf66239b7d676e7
gwoo <gwoohoo@gmail.com>
2011-08-25 15:51:32 -0700

    adding mongo connection

--- config/bootstrap/connections.php
+++ config/bootstrap/connections.php
@@ -9,15 +9,24 @@
 use lithium\data\Connections;
 
 Connections::add('default', array(
-	'adapter' => 'MongoDb',
-	'host' => 'localhost',
-	'database' => 'lithosphere'
+	'type' => 'MongoDb',
+	'host' => 'flame.mongohq.com:27094',
+	'database' => 'orchestra_mongo_33268d7b',
+	'login' => 'orchestra',
+	'password' => '635eb212ca173a1d53a64316f1035523',
+	'timeout' => 300
 ));
 
-Connections::add('test', array(
-	'adapter' => 'MongoDb',
-	'host' => 'localhost',
-	'database' => 'lithosphere_test'
-));
+// Connections::add('default', array(
+// 	'adapter' => 'MongoDb',
+// 	'host' => 'localhost',
+// 	'database' => 'lithosphere'
+// ));
+
+// Connections::add('test', array(
+// 	'adapter' => 'MongoDb',
+// 	'host' => 'localhost',
+// 	'database' => 'lithosphere_test'
+// ));
 
 ?>
