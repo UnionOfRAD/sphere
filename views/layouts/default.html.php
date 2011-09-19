9591f3ee5f51470bfcadccaee79fa1e6ac9a8bb9
Jon Adams <pointlessjon@gmail.com>
2011-09-19 00:07:17 -0700

    adding google analytics

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -271,5 +271,18 @@
 		});
 	</script>
 	<?php if (isset($this->viewJs)) echo $this->viewJs; ?>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-1630573-10']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </body>
 </html>
