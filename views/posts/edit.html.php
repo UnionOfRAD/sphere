61a598ca4ef49a6cfe4ba40f9eadb3a92247b439
gwoo <gwoohoo@gmail.com>
2012-12-01 20:57:37 -0800

    Fix tags.

--- views/posts/edit.html.php
+++ views/posts/edit.html.php
@@ -19,15 +19,21 @@
 	<label for="PostTitle">Title:</label>
 	<?php echo $this->form->text('title', array('id' => 'PostTitle')); ?>
 	<label for="PostContent">Content:</label>
-	<?php echo $this->form->textarea('content', array('id' => 'PostContent', 'class' => 'custom-vertical-scroll')); ?>
+	<?php echo $this->form->textarea('content', array(
+		'id' => 'PostContent', 'class' => 'custom-vertical-scroll'
+	)); ?>
 	<div class="base-tags">
 		<label class="label">Primary Tag:</label>
 		<?php foreach ($tags as $tag) { ?>
-			<?php echo $this->html->link($tag, "/{$tag}", array('title' => $tag, 'class' => "icon tag $tag", 'data-tag' => $tag)); ?>
+			<?php echo $this->html->link($tag, "/{$tag}", array(
+				'title' => $tag, 'class' => "icon tag $tag", 'data-tag' => $tag
+			)); ?>
 		<?php } ?>
 	</div>
 	<label for="PostTags" class="tags">Tags:</label>
-	<?php echo $this->form->text('tags', array('id' => 'PostTags')); ?>
+	<?php echo $this->form->text('tags', array(
+		'id' => 'PostTags',  'value' => ($post->tags) ? join(', ', $post->tags->to('array')) : null
+	)); ?>
 	<small class="post-notes">separate tags with commas.</small>
 	<?php echo $this->form->submit('save'); ?>
 	<?php echo $this->form->end(); ?>
