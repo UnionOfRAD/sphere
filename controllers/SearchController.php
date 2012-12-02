fef81e4d02eb2d99c390449227b583d25e003ec8
gwoo <gwoohoo@gmail.com>
2012-12-01 20:49:55 -0800

    Fix latest.

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -110,7 +110,7 @@ class SearchController extends \lithium\action\Controller {
 		);
 
 		$conditions = compact('created');
-		$order = array('created' => -11, 'rating' => -1);
+		$order = array('created' => -1, 'rating' => -1);
 
 		$url = $this->request->params;
 
